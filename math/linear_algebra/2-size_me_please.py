675e609eb1655bdcc1ba221022f74408f4b82b5b
korneel <korneel.loy@gmail.com>
2026-03-30 16:09:20 +0200

    ex 2 corr 2 doc

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
-"""returns shape of matrix"""
+"""Module for matrix shape"""
 
 
 def matrix_shape(matrix):
+    """Returns the shape of a matrix."""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
