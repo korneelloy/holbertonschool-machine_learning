cb463a7e66bdab9d2ca2d37a3b7e5e9dfa69a540
korneel <korneel.loy@gmail.com>
2026-03-30 16:40:58 +0200

    ex 3

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,17 @@
+"""module for flipping matrices over their diagonal"""
+
+
+def matrix_transpose(matrix):
+    """Returns the transposed version of a 2D matrix."""
+    reversed_matrix = []
+    i = 0
+    while i < len(matrix[0]):
+        row = []
+        j = 0
+        while j < len(matrix):
+            row.append(matrix[j][i])
+            j += 1
+        reversed_matrix.append(row)
+        i += 1
+
+    return reversed_matrix
