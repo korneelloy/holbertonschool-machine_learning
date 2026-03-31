f271a377b6d31157d64a905ae09d1ad9d3e2ff6d
korneel <korneel.loy@gmail.com>
2026-03-31 14:38:09 +0200

    ex 11 correrction sans import

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -3,10 +3,8 @@
 
 """transpose matrix"""
 
-import numpy as np
-
 
 def np_transpose(matrix):
     """function that transposes matrix"""
-    a = np.transpose(matrix)
+    a = matrix.transpose()
     return a
