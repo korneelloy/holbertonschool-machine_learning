b568de584101bca144f424e8553e37eb395b1081
korneel <korneel.loy@gmail.com>
2026-03-31 14:29:56 +0200

    ex 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""transpose matrix"""
+
+import numpy as np
+
+
+def np_transpose(matrix):
+    """function that transposes matrix"""
+    a = np.transpose(matrix)
+    return a
