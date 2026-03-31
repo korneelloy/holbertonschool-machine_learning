f59e659151914dcb009cb9d3d89ed6ce4e0de3f7
korneel <korneel.loy@gmail.com>
2026-03-31 14:59:43 +0200

    ex 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+"""matrix multiplication"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """matrix multiplication"""
+    a = np.matmul(mat1, mat2)
+    return a
