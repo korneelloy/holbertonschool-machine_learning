8ec57fc496f3258c22686870729330e41ae02f4b
korneel <korneel.loy@gmail.com>
2026-03-31 12:07:43 +0200

    ex7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+"""concatenate 2 matrices along axe"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenate 2 matrices along axis, default 0"""
+    if axis == 0:
+        return mat1 + mat2
+    new = []
+    i = 0
+    while i < len(mat1):
+        line = mat1[i] + mat2[i]
+        new.append(line)
+        i += 1
+    return new
