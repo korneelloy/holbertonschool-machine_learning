80bb82d0c6d760adca101dfa0eaab6458fae20cf
korneel <korneel.loy@gmail.com>
2026-03-31 12:22:48 +0200

    ex7 condition changed

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,10 +6,12 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenate 2 matrices along axis, default 0"""
-    if len(mat1) != len(mat2) and len(mat1) != len(mat2[0]):
-        return None
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return mat1 + mat2
+    if len(mat1) != len(mat2):
+        return None
     new = []
     i = 0
     while i < len(mat1):
--- math/linear_algebra/7-main.py
+++ math/linear_algebra/7-main.py
@@ -3,7 +3,7 @@
 cat_matrices2D = __import__("7-gettin_cozy").cat_matrices2D
 
 mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6, 7]]
+mat2 = [[5, 6]]
 mat3 = [[7], [8]]
 mat4 = cat_matrices2D(mat1, mat2)
 mat5 = cat_matrices2D(mat1, mat3, axis=1)
