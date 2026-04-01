f644454b6215ddbcedbf2d1c999e31007258017b
korneel <korneel.loy@gmail.com>
2026-04-01 10:20:51 +0200

    ex 0 doc

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 
 def line():
-    """plotting"""
+    """plotting Y solid red / x range 0-10"""
     x = np.arange(0, 11)
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
