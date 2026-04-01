b63b75667fb5b2426d1b250438e912758c0ee582
korneel <korneel.loy@gmail.com>
2026-04-01 10:19:50 +0200

    ex 0

---
+++ math/plotting/0-line.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+"""plotting Y solid red / x range 0-10"""
+
+
+def line():
+    """plotting"""
+    x = np.arange(0, 11)
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 10)
+    plt.plot(x, y, "r")
+    plt.show()
---
+++ math/plotting/README.md
@@ -0,0 +1,1 @@
+plotting Holberton ex
