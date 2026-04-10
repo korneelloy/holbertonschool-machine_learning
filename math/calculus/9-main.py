5d4e5c5bb28bc9fe3f1598b6ce6e1bac86abfe0e
korneel <korneel.loy@gmail.com>
2026-04-10 15:59:02 +0200

    ex 9 exclude 1

--- math/calculus/9-main.py
+++ math/calculus/9-main.py
@@ -2,5 +2,5 @@
 
 summation_i_squared = __import__("9-sum_total").summation_i_squared
 
-n = 6.2
+n = 2
 print(summation_i_squared(n))
--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """return sum of squared number"""
-    if not isinstance(n, (int)):
+    if not isinstance(n, int) or n < 1:
         return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
