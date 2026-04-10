dd2b8b475d8f1d458b42bd64faa0a33150fa4a6a
korneel <korneel.loy@gmail.com>
2026-04-10 15:51:16 +0200

    ex 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,8 @@
+"""sum of powers"""
+
+
+def summation_i_squared(n):
+    """return sum of squared number"""
+    if not isinstance(n, (int, float)):
+        return None
+    return int(n * (n + 1) * (2 * n + 1) / 6)
