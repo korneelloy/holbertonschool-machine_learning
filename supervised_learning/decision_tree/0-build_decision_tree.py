f13e8189fb14990e5dc74027f6459bb39197e07e
korneel <korneel.loy@gmail.com>
2026-04-20 13:20:16 +0200

    ex 0 pycodestyle

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,8 +2,10 @@
 
 import numpy as np
 
+
 class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -13,8 +15,9 @@ class Node:
         self.sub_population = None
         self.depth = depth
 
-    def max_depth_below(self) :
-        return max(self.left_child.max_depth_below(), self.right_child.max_depth_below())
+    def max_depth_below(self):
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
 
 
 class Leaf(Node):
@@ -24,11 +27,14 @@ class Leaf(Node):
         self.is_leaf = True
         self.depth = depth
 
-    def max_depth_below(self) :
+    def max_depth_below(self):
         return self.depth
 
+
 class Decision_Tree():
-    def __init__(self, max_depth=10, min_pop=1, seed=0, split_criterion="random", root=None):
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random",
+                 root=None):
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -41,6 +47,5 @@ class Decision_Tree():
         self.split_criterion = split_criterion
         self.predict = None
 
-    def depth(self) :
+    def depth(self):
         return self.root.max_depth_below()
-    
