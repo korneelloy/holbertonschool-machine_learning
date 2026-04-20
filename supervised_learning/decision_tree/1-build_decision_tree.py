d182de718e1aaa571464a663a3182a459bbe678b
korneel <korneel.loy@gmail.com>
2026-04-20 13:46:39 +0200

    ex 1

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+
+"""build descions tree"""
+
+
+import numpy as np
+
+
+class Node:
+    """node in tree"""
+
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """returns max depth below this node"""
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+    def count_nodes_below(self, only_leaves=False):
+        """counts nb of nodes below"""
+        if only_leaves == True:
+            return (self.left_child.count_nodes_below(only_leaves=only_leaves)
+                    + self.right_child.count_nodes_below(only_leaves=only_leaves))
+        else:
+            return (1 + self.left_child.count_nodes_below(only_leaves=only_leaves)
+                    + self.right_child.count_nodes_below(only_leaves=only_leaves))
+
+
+class Leaf(Node):
+    """leaf in tree"""
+
+    def __init__(self, value, depth=None):
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """returns max depth below this leaf"""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """counts nb of nodes below"""
+        return 1
+
+
+class Decision_Tree():
+    """class tree itself"""
+
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random",
+                 root=None):
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """returens max depth of tree"""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """counts nb of nodes below"""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
