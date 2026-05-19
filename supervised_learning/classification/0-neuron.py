ef0a409d0ddce1b3d300f0dc746a6eeff72ca4b6
korneel <korneel.loy@gmail.com>
2026-05-19 11:13:38 +0200

    ex 0 comment

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
+"""Module defining a single neuron for binary classification."""
 
 import numpy as np
 
-"""
-class Neuron : single neuron performing binary classification
-@nx : the number of input features to the neuron
-Public instance attributes:
-W: The weights vector for the neuron.
-b: The bias for the neuron.
-A: The activated output of the neuron (prediction)
-"""
-
 
 class Neuron:
-    """neuron class for binary classificationin tree"""
+    """Single neuron performing binary classification.
+        @nx : the number of input features to the neuron
+        Public instance attributes:
+        W: The weights vector for the neuron.
+        b: The bias for the neuron.
+        A: The activated output of the neuron (prediction)
+    """
 
     def __init__(self, nx):
+        """Initialize Neuron with nx input features."""
         if not isinstance(nx, int) or isinstance(nx, bool):
             raise TypeError("nx must be an integer")
         elif nx < 1:
