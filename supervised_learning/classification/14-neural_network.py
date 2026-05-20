23a02b9bb3a3da16e011bc4d7db23c41b7de1311
korneel <korneel.loy@gmail.com>
2026-05-20 13:42:07 +0200

    ex 14

--- supervised_learning/classification/14-neural_network.py
+++ supervised_learning/classification/14-neural_network.py
@@ -118,7 +118,7 @@ class NeuralNetwork:
 
         dZ2 = A2 - Y
         dW2 = (1/m) * np.dot(dZ2, A1.T)
-        db2 = (1/m) * np.sum(dZ2)
+        db2 = (1/m) * np.sum(dZ2, axis=1, keepdims=True)
 
         dZ1 = np.dot(self.W2.T, dZ2) * A1 * (1 - A1)
         dW1 = (1/m) * np.dot(dZ1, X.T)
