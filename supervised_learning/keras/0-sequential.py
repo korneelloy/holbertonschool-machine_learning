bd78c8a98597dcd46fb75031b1b021d6bef04b56
korneel <korneel.loy@gmail.com>
2026-06-12 11:37:33 +0200

    ex 0

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
-import tensorflow.keras as K
+"""constructing neural network"""
 
-"""
-  function that builds a neural network with the Keras library
-  nx: number of input features
-  layers: list containing the number of nodes in each layer
-  activations: list containing the activation functions
-  lambtha: L2 regularization parameter
-  keep_prob: probability node will be kept for dropout
-  Returns: the keras model
-"""
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
+    """
+      function that builds a neural network with the Keras library
+      nx: number of input features
+      layers: list containing the number of nodes in each layer
+      activations: list containing the activation functions
+      lambtha: L2 regularization parameter
+      keep_prob: probability node will be kept for dropout
+      Returns: the keras model
+    """
     model = K.Sequential()
     for i in range(len(layers)):
         if i == 0:
