5824467508d0ea46e9abad0e8d161321ceaa57eb
korneel <korneel.loy@gmail.com>
2026-06-12 17:13:42 +0200

    ex 9

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""load and save model"""
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """
+    saves an entire model:
+    network is the model to save
+    filename is the path of the file that the model should be saved to
+    Returns: None
+    """
+    network.save(filename)
+    return None
+
+
+def load_model(filename):
+    """
+    loads an entire model:
+    filename is the path of the file that the model should be loaded from
+    Returns: the loaded model
+    """
+    return K.models.load_model(filename)
