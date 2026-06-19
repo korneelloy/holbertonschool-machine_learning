fd7ad0a21a2f5f652b0c637a250fce2eacb27556
korneel <korneel.loy@gmail.com>
2026-06-19 09:43:48 +0200

    ex 10

--- supervised_learning/keras/10-weights.py
+++ supervised_learning/keras/10-weights.py
@@ -2,28 +2,25 @@
 
 import tensorflow.keras as K
 
-"""
-  saves a model's weights
-  network is the model whose weights should be saved
-  filename is the path of the file that the weights should be saved to
-  save_format is the format in which the weights should be saved
-  Returns: None
-"""
-
 
 def save_weights(network, filename, save_format='keras'):
+    """
+      saves a model's weights
+      network is the model whose weights should be saved
+      filename is the path of the file that the weights should be saved to
+      save_format is the format in which the weights should be saved
+      Returns: None
+    """
     network.save(filename, save_format=save_format)
     return None
 
 
-"""
-  loads a model's weights
-  network is the model to which the weights should be loaded
-  filename is the path of the file that the weights should be loaded from
-  Returns: None
-"""
-
-
 def load_weights(network, filename):
+    """
+      loads a model's weights
+      network is the model to which the weights should be loaded
+      filename is the path of the file that the weights should be loaded from
+      Returns: None
+    """
     network.load_weights(filename)
     return None
