b66dd11f9d035a64e3e22269037d8090b52f416c
korneel <korneel.loy@gmail.com>
2026-06-19 10:33:03 +0200

    ex 13

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""predeiction with a neural network"""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+      makes a prediction using a neural network:
+      network is the network model to make the prediction with
+      data is the input data to make the prediction with
+      verbose is a boolean that determines if output should be printed
+      Returns: the prediction for the data
+    """
+    return (network.predict(data, verbose=verbose))
