6cc4f74ae74336656810318ca11a007b781232fe
korneel <korneel.loy@gmail.com>
2026-07-05 15:26:38 +0200

    ex 12

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -18,6 +18,7 @@ def learning_rate_decay(alpha, decay_rate, decay_step):
     lr_schedule = tf.keras.optimizers.schedules.InverseTimeDecay(
         initial_learning_rate=alpha,
         decay_steps=decay_step,
-        decay_rate=decay_rate
+        decay_rate=decay_rate,
+        staircase=True
     )
     return lr_schedule
