e0eb4920a27d22cd68f2ba7753500df6caf2809a
korneel <korneel.loy@gmail.com>
2026-07-24 13:56:38 +0200

    ex 0

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+"""cost of a neural network with L2 regularization:"""
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """
+    calculates the cost of a neural network with L2 regularization
+
+    cost is the cost of the network without L2 regularization
+    lambtha is the regularization parameter
+    weights is a dictionary of the weights and biases
+    (numpy.ndarrays) of the neural network
+    L is the number of layers in the neural network
+    m is the number of data points used
+    Returns: the cost of the network accounting for L2 regularization
+    """
+
+    weights_norm = 0
+    for i in range(1, L + 1):
+        W = weights['W' + str(i)]
+        weights_norm += np.linalg.norm(W) ** 2
+
+    return cost + (lambtha / (2 * m)) * weights_norm
