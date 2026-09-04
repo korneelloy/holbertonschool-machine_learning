0b756a8907dbb7d9ea2c6f1f5f6b08c844ec1609
korneel <korneel.loy@gmail.com>
2026-09-04 15:03:42 +0200

    ex 2

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -30,7 +30,7 @@ def resnet50():
         64, (7, 7), strides=2, padding='same',
         kernel_initializer=K.initializers.HeNormal(seed=0))(inputs)
     A = K.layers.BatchNormalization(axis=3)(A)
-    A = K.layers.Activation('relu')(A)
+    A = K.layers.ReLU()(A)
 
     # pool : 3×3, stride 2
     A = K.layers.MaxPooling2D((3, 3), strides=2, padding='same')(A)
